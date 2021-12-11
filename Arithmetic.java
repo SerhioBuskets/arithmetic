5f491a6ad19d938611b93d0bf706d802a3fe7d6c
SerhioBuskets <86625225+SerhioBuskets@users.noreply.github.com>
2021-12-11 16:34:48 +0300

    changed

--- Arithmetic.java
+++ Arithmetic.java
@@ -2,46 +2,36 @@ public class Arithmetic {
 
     int length;
     int height;
-    int perimeter;
-    int square;
-    int max;
-    int min;
+
 
     public Arithmetic(int length, int height) {
         this.length = length;
         this.height = height;
     }
 
-    public void calculatePerimeter() {
-        perimeter = (length + height) * 2;
-        System.out.println("Вместо суммы, как в задании, я нахожу периметр (сумма чисел, умноженная на 2) = " + perimeter);
+    public int getPerimeter() {
+        return (length + height) * 2;
     }
 
-    public void calculateSquare() {
-        square = length * height;
-        System.out.println("Площадь или произведение чисел = " + square);
+    public int getSquare() {
+        return length * height;
     }
 
-    public void max() {
+    public int getMax() {
         if (length >= height) {
-            max = length;
-            System.out.println(length + " Длина больше ширины");
+            return length;
         } else {
-            max = height;
-            System.out.println(height + " Ширина больше длины");
+            return height;
         }
     }
 
-    public void min() {
+    public int getMin() {
         if (length <= height) {
-            min = length;
-            System.out.println(length + " Длина меньше ширины");
+            return length;
         } else {
-            min = height;
-            System.out.println(height + " Ширина меньше длины");
+            return height;
         }
     }
-
 
     public void print(String result) {
         System.out.println(result);
--- main.java
+++ main.java
@@ -2,12 +2,16 @@ public class main {
 
     public static void main(String[] args) {
 
-        Arithmetic quadrate = new Arithmetic(5,2);
+        Arithmetic quadrate = new Arithmetic(7,1);
         quadrate.print("Расчет");
-        quadrate.calculatePerimeter();
-        quadrate.calculateSquare();
-        quadrate.max();
-        quadrate.min();
+        int perimeter = quadrate.getPerimeter();
+        int square = quadrate.getSquare();
+        int max = quadrate.getMax();
+        int min = quadrate.getMin();
 
+        System.out.println("периметр = " + perimeter);
+        System.out.println("площадь = " + square);
+        System.out.println("максимальное значение = " + max);
+        System.out.println("минмиальное значение = " + min);
     }
 }
